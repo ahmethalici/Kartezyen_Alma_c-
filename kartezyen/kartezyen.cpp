37bf7a29e5434494c66a7f66ba7c367866f0e496
ahmethalici <33725758+ahmethalici@users.noreply.github.com>
2017-12-14 19:46:24 +0300

    Kodlar
    Programın Kondları

---
+++ kartezyen/kartezyen.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	
+	for (int x=0; x<3;x++)
+	{
+		for(int y=0; y<3;y++)
+		{
+			cout<<x<<y<<endl;
+	    }
+    }
+	
+	return 0;
+}
